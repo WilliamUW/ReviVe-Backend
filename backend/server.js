c09a737a648ace1be360560b46f8bb7ef15fc66e
Wes Jorgensen <wjorgensen29@gmail.com>
2024-06-29 15:31:33 -0700

    Full test coverage

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = 3001;
+const port = 3002;
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const VET_ID = "vechain"; // This is the slug for VeChain on CoinMarketCap
